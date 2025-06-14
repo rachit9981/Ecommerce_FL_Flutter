7c489b59cb6d7ed4896f9e7aa8d45715b4c12e2c
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-06-14 13:27:54 +0530

    Fix API URL and add missing newline in config; ensure proper formatting in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/user_provider.dart';
 import 'providers/cart_provider.dart'; // Import CartProvider
+
 void main() {
   runApp(MyApp());
 }
--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,1 +1,1 @@
-const String apiUrl = "http://69.62.72.199:8000/api";
+const String apiUrl = "http://10.0.2.2:8000/api";
